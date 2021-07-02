99f36c7dc0753f9fac69793178c57399c5c98caa
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2021-07-02 11:54:59 -0300

    Meu primeiro projeto - Springboot/java/gradle

---
+++ src/main/java/br/com/exemplo/myprojectgradle/MyprojectGradleApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.myprojectgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemplo/myprojectgradle/MyprojectGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.myprojectgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
